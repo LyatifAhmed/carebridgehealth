3724c4698d22010315b1ee7c94dc11444665c0c1
LyatifAhmed <latifmutlu@gmail.com>
2026-03-29 21:42:19 +0100

    Add SEO assets and Microsoft Clarity

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://carebridgehealth.com";
+  const baseUrl = "https://carebridgehealth.co";
 
   return [
     {
