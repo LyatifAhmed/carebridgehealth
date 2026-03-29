5312edbf7dc8cc2de6dcf72d000947e2a9b477d0
LyatifAhmed <latifmutlu@gmail.com>
2026-03-29 21:43:17 +0100

    Add SEO assets and Microsoft Clarity

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://carebridgehealth.com/sitemap.xml",
+    sitemap: "https://carebridgehealth.co/sitemap.xml",
   };
 }
